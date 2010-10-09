bfe2981c545f58f50130f14d4a3183580e318e66
andriy.gerasika@gmail.com <andriy.gerasika@gmail.com@1be7dbee-bbc4-11dd-ab14-3be0eea8eb7e>
2010-10-09 11:12:27 +0000

    ukrainian translation plus fixes to russian translation

--- branches/1.2/js/openid-jquery-uk.js
+++ branches/1.2/js/openid-jquery-uk.js
@@ -24,29 +24,29 @@ var providers_large = {
     },
     myopenid: {
       name: 'MyOpenID',
-      label: 'Введите ваше имя пользователя на MyOpenID.',
+      label: 'Введіть ваше ім\'я користувача на MyOpenID.',
       url: 'http://{username}.myopenid.com/'
     }
 };
 var providers_small = {
     openid: {
       name: 'OpenID',     
-      label: 'Введите ваш OpenID.',
+      label: 'Введіть ваш OpenID.',
       url: null
     },
     livejournal: {
         name: 'Живой Журнал',
-        label: 'Введите ваше имя в Живом Журнале.',
+        label: 'Введіть ваше ім\'я в Живому Журналі.',
         url: 'http://{username}.livejournal.com/'
     },
     flickr: {
         name: 'Flickr',        
-        label: 'Введите ваше имя на Flickr.',
+        label: 'Введіть ваше ім\'я на Flickr.',
         url: 'http://flickr.com/{username}/'
     },
     wordpress: {
         name: 'Wordpress',
-        label: 'Введите ваше имя на Wordpress.com.',
+        label: 'Введіть ваше ім\'я на Wordpress.com.',
         url: 'http://{username}.wordpress.com/'
     },
     blogger: {
@@ -56,15 +56,15 @@ var providers_small = {
     },
     verisign: {
       name: 'Verisign',
-      label: 'Ваше имя пользователя на Verisign',
+      label: 'Ваше ім\'я користувача на Verisign',
       url: 'http://{username}.pip.verisignlabs.com/'
     },
     google_profile: {
       name: 'Профиль Google',
-      label: 'Введите ваше имя на Google Profile',
+      label: 'Введіть ваше ім\'я на Google Profile',
       url: 'http://www.google.com/profiles/{username}'
     }
 };
 
-openid.demo_text = 'В демонстрационном режиме на клиенте. В действительности произошел бы сабмит следующего OpenID:';
-openid.signin_text = 'Войти';
+openid.demo_text = 'В демонстраційному режимі на клієнті. Насправді пройшов би сабміт наступного OpenID:';
+openid.signin_text = 'Ввійти';
